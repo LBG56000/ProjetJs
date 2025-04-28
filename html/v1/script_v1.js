354165854330e8bf71c5942492a3afb2c2a66146
LBG56000 <lbgzorro2005@gmail.com>
2025-04-28 22:34:15 +0200

    add v1

---
+++ html/v1/script_v1.js
@@ -0,0 +1,56 @@
+/**
+ * Récupération de l'ensemble des pays
+ */
+const all_countries = Country.all_countries
+
+/**
+ * L'ensembles des colonnes à ce jour
+ */
+const colums = ['Nom Français', 'Population','Surface','Densité de population','Continent d\'appartenance','Drapeau']
+
+/**
+ * Ajout d'un tableau sur la page html
+ */
+$("body").append("<table></table>")
+
+/**
+ * Ajout d'un haut de tableau
+ */
+$("table").append("<thead></thead>")
+
+/**
+ * Ajout d'une ligne au table
+ */
+$("thead").append("<tr></tr>")
+
+/**
+ * Ajout des noms de colonne
+ */
+
+$.map(colums, (colum) => {
+    $("thead > tr").append(`<th>${colum}</th>`)
+})
+
+/**
+ * Ajout d'un corps de tableau
+ */
+$("table").append("<tbody></tbody>")
+
+/**
+ * Ajout des pays dans le corps du tableau
+ */
+$.map(Object.values(all_countries), (country) => {
+
+    // Création de la ligne avec les différentes information demandées
+    const countryLine = `<tr>
+        <td>${country.frenchName || 'N/A'}</td>
+        <td>${country.population || 'N/A'}</td>
+        <td>${country.area || 'N/A'}</td>
+        <td>${country.getPopDensity() || 'N/A'}</td>
+        <td>${country.region || 'N/A'}</td>
+        <td><img src=${country.linkToImage} alt=Drapeau_${country.frenchName} width="130" height="75" title=Drapeau_${country.frenchName}></img></td>
+    </tr>`
+
+    // Ajout de la ligne au corps du tableau
+    $("tbody").append(countryLine)
+})
