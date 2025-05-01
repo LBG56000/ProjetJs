f0fb546fda411dbe002f97626147ca9c9e78d6f1
LBG56000 <lbgzorro2005@gmail.com>
2025-05-01 11:12:04 +0200

    add function to add countries table

--- html/v1/script_v1.js
+++ html/v1/script_v1.js
@@ -9,48 +9,47 @@ const all_countries = Country.all_countries
 const colums = ['Nom Français', 'Population','Surface','Densité de population','Continent d\'appartenance','Drapeau']
 
 /**
- * Ajout d'un tableau sur la page html
+ * Fonction permattant d'ajouter le tableau avec les différents pays
  */
-$("body").append("<table></table>")
+function printCountriesTable() {
 
-/**
- * Ajout d'un haut de tableau
- */
-$("table").append("<thead></thead>")
+    // Ajout d'un tableau sur la page html
+    $("body").append("<table></table>")
+    
 
-/**
- * Ajout d'une ligne au table
- */
-$("thead").append("<tr></tr>")
+    // Ajout d'un haut de tableau
+    $("table").append("<thead></thead>")
+    
 
-/**
- * Ajout des noms de colonne
- */
+    // Ajout d'une ligne au table
+    $("thead").append("<tr></tr>")
+    
 
-$.map(colums, (colum) => {
-    $("thead > tr").append(`<th>${colum}</th>`)
-})
+    // Ajout des noms de colonne
+    $.map(colums, (colum) => {
+        $("thead > tr").append(`<th>${colum}</th>`)
+    })
+    
+    // Ajout d'un corps de tableau
+    $("table").append("<tbody></tbody>")
+    
+    // Ajout des pays dans le corps du tableau
+    $.map(Object.values(all_countries), (country) => {
+    
+        // Création de la ligne avec les différentes information demandées
+        const countryLine = `<tr>
+            <td>${country.frenchName || 'N/A'}</td>
+            <td>${country.population || 'N/A'}</td>
+            <td>${country.area || 'N/A'}</td>
+            <td>${country.getPopDensity() || 'N/A'}</td>
+            <td>${country.region || 'N/A'}</td>
+            <td><img src=${country.linkToImage} alt=Drapeau_${country.frenchName} width="130" height="75" title=Drapeau_${country.frenchName}></img></td>
+        </tr>`
+    
+        // Ajout de la ligne au corps du tableau
+        $("tbody").append(countryLine)
+    })
+}
 
-/**
- * Ajout d'un corps de tableau
- */
-$("table").append("<tbody></tbody>")
-
-/**
- * Ajout des pays dans le corps du tableau
- */
-$.map(Object.values(all_countries), (country) => {
-
-    // Création de la ligne avec les différentes information demandées
-    const countryLine = `<tr>
-        <td>${country.frenchName || 'N/A'}</td>
-        <td>${country.population || 'N/A'}</td>
-        <td>${country.area || 'N/A'}</td>
-        <td>${country.getPopDensity() || 'N/A'}</td>
-        <td>${country.region || 'N/A'}</td>
-        <td><img src=${country.linkToImage} alt=Drapeau_${country.frenchName} width="130" height="75" title=Drapeau_${country.frenchName}></img></td>
-    </tr>`
-
-    // Ajout de la ligne au corps du tableau
-    $("tbody").append(countryLine)
-})
+// Appel de la fonction permettant d'ajouter la table
+printCountriesTable()
