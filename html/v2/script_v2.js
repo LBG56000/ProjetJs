2506b893f8e451033612225c12b31a6ab0b0a1d2
LBG56000 <lbgzorro2005@gmail.com>
2025-05-01 11:33:57 +0200

    add v2

--- html/v2/script_v2.js
+++ html/v2/script_v2.js
@@ -4,74 +4,107 @@
 const all_countries = Country.all_countries
 
 /**
- * Définition de la page actuelle
+ * Création de la constante sur le nombre d'élément par page
  */
-let pageNumber = 0
+const elements_per_page = 25
 
 /**
- * Définition de la du tableau diviser 
+ * Création de la variable contenant la page actuelle
  */
-
-let allCountriesSliced =Object.values(all_countries).slice(0,25)
+let pageNumber = -1
 
 /**
  * L'ensembles des colonnes à ce jour
- */
-const colums = ['Nom Français', 'Population','Surface','Densité de population','Continent d\'appartenance','Drapeau']
+*/
+const colums = ['Nom Français', 'Population', 'Surface', 'Densité de population', 'Continent d\'appartenance', 'Drapeau']
 
 /**
  * Définition des boutons à utiliser
- */
-const buttons = ['PRÉC.','SUIV.']
+*/
+const buttons = ['PRÉC', 'SUIV']
 
-/**
- * Ajout d'un tableau sur la page html
- */
+
+// Ajout d'un tableau sur la page html
 $("body").append("<table></table>")
 
-/**
- * Ajout d'un haut de tableau
- */
+// Ajout d'un haut de tableau
 $("table").append("<thead></thead>")
 
-/**
- * Ajout d'une ligne au table
- */
+// Ajout d'une ligne au table
 $("thead").append("<tr></tr>")
 
-/**
- * Ajout des noms de colonne
- */
-
+// Ajout des noms de colonne
 $.map(colums, (colum) => {
     $("thead > tr").append(`<th>${colum}</th>`)
 })
 
 /**
- * Ajout d'un corps de tableau
+ * Fonction permattant d'ajouter le tableau avec les différents pays
  */
-$("table").append("<tbody></tbody>")
+function printCountriesTable() {
+    
+    // Ajout d'un corps de tableau
+    $("table").append("<tbody></tbody>")
+
+    const allCountriesSliced = Object.values(all_countries).slice(pageNumber * elements_per_page, (pageNumber + 1) * elements_per_page)
+    
+    // Ajout des pays dans le corps du tableau
+    $.map(Object.values(allCountriesSliced), (country) => {
+    
+        // Création de la ligne avec les différentes information demandées
+        const countryLine = `<tr>
+            <td>${country.frenchName || 'N/A'}</td>
+            <td>${country.population || 'N/A'}</td>
+            <td>${country.area || 'N/A'}</td>
+            <td>${country.getPopDensity() || 'N/A'}</td>
+            <td>${country.region || 'N/A'}</td>
+            <td><img src=${country.linkToImage} alt=Drapeau_${country.frenchName} width="130" height="75" title=Drapeau_${country.frenchName}></img></td>
+        </tr>`
+    
+        // Ajout de la ligne au corps du tableau
+        $("tbody").append(countryLine)
+    })
+}
 
 /**
- * Ajout des pays dans le corps du tableau
+ * Fonction permattant à l'utilisateur de faire défiler les pages
  */
-$.map(Object.values(allCountriesSliced), (country) => {
+function determinePageNumber (actionButton){
+    switch (actionButton) {
+        case 'PRÉC':
+            if (pageNumber > 0) pageNumber -= 1
+            break
 
-    // Création de la ligne avec les différentes information demandées
-    const countryLine = `<tr>
-        <td>${country.frenchName || 'N/A'}</td>
-        <td>${country.population || 'N/A'}</td>
-        <td>${country.area || 'N/A'}</td>
-        <td>${country.getPopDensity() || 'N/A'}</td>
-        <td>${country.region || 'N/A'}</td>
-        <td><img src=${country.linkToImage} alt=Drapeau_${country.frenchName} width="130" height="75" title=Drapeau_${country.frenchName}></img></td>
-    </tr>`
+        case 'SUIV':
+            if ((pageNumber + 1) * elements_per_page < Object.values(all_countries).length) {
+                pageNumber += 1
+            }
+            break
+        default:
+            break
+    }
+    $("tbody").remove()
+    printCountriesTable()
+}
 
-    // Ajout de la ligne au corps du tableau
-    $("tbody").append(countryLine)
-})
+/**
+ * Condition permettant de n'appeller qu'une seule fois la fonction d'affichage
+ */
+if (pageNumber === -1) {
+    // Appel de la fonction permettant d'ajouter la table
+    pageNumber = 0
+    printCountriesTable()
+}
 
+/**
+ * Ajout du container des boutons
+ */
 $("body").append("<div class=button_container></div>")
-$.map(buttons, (button) => {
-    $(".button_container").append(`<button onClick=alert('couc')>${button}</button>`);
-})
+
+/**
+ * Ajout des boutons dans le container
+ */
+$(".button_container").append(`<button onClick=determinePageNumber('${buttons[0]}')>${buttons[0]}</button>`)
+$(".button_container").append(`<button onClick=determinePageNumber('${buttons[1]}')>${buttons[1]}</button>`)
+
+
